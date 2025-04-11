1440682e95f5e77c6089c49f862159f3cf2394da
Murthy Karra <karrad@Murthys-MacBook-Pro-2.local>
2025-04-11 09:21:41 -0700

    repackaged charts

--- update-index.sh
+++ update-index.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-helm repo index . --url https://nstarx.github.io/helm
-echo index updated.
+# Package charts from charts directory
+for dir in charts/*/; do
+  # For standard chart structure
+  if [ -f "$dir/Chart.yaml" ]; then
+    helm package "$dir" -d releases/
+  # For nested chart structure
+  elif [ -d "$dir/$(basename "$dir")" ] && [ -f "$dir/$(basename "$dir")/Chart.yaml" ]; then
+    helm package "$dir/$(basename "$dir")" -d releases/
+  # For spark-admin structure
+  elif [ -d "$dir/chart" ] && [ -f "$dir/chart/Chart.yaml" ]; then
+    helm package "$dir/chart" -d releases/
+  fi
+done
+
+# Update index.yaml
+helm repo index releases/ --url https://murthy-karra.github.io/helm/releases
+mv releases/index.yaml .
